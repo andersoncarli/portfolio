b47d38598b1418f95894157877d0c828b8d1ca41
Anderson Carli <anderson.carli@gmail.com>
2024-06-15 21:10:39 -0300

    cleanup to defaults

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,12 @@
+import Link from 'next/link'
+
 export default function AboutPage() {
   return (
     <section className="py-20">
-      <h2 className="text-4xl font-bold mb-4">About Me</h2>
-      <p className="text-lg leading-relaxed">
-        I am a seasoned software engineer with a passion for building efficient and scalable web applications...
-      </p>
+      <h2>About Me</h2>
+      <p>I am a seasoned software engineer with a passion for building efficient and scalable web applications.</p>
+      <p>This portfolio was built using NextJS, TailwindCSS and is hosted at Vercel.</p>
+      <p>You can access its souce code <strong><Link href="https://github.com/andersoncarli/portfolio">Here</Link></strong></p>
     </section>
   );
 }
